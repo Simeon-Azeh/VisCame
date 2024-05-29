95e884ff2ed3186bb3ba421b6739b15a6ea008c0
Simeon-Azeh <144020513+Simeon-Azeh@users.noreply.github.com>
2024-05-29 08:46:53 +0100

    Done with nav

---
+++ script.js
@@ -0,0 +1,36 @@
+window.addEventListener('load', function() {
+    const loader = document.getElementById('loader');
+    const content = document.querySelector('.content');
+    const navbar = document.querySelector('.navbar');
+    
+    loader.style.display = 'none';
+    content.style.display = 'block';
+    
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 0) {
+            navbar.classList.add('shadow');
+        } else {
+            navbar.classList.remove('shadow');
+        }
+    });
+});
+
+//Mobile media queries
+// Get the mobile menu button, the mobile navigation menu, and the close icon
+const mobileMenuButton = document.getElementById('mobile_menu');
+const mobileNavMenu = document.querySelector('.mobile_nav_menu');
+const mobileMenuClose = document.getElementById('mobile_menu_close');
+
+// Add click event listener to the mobile menu button
+mobileMenuButton.addEventListener('click', function() {
+    // Toggle the 'active' class on the mobile navigation menu
+    mobileNavMenu.classList.toggle('active');
+});
+
+// Add click event listener to the close icon
+mobileMenuClose.addEventListener('click', function() {
+    // Hide the mobile navigation menu
+    mobileNavMenu.classList.remove('active');
+});
+
+
